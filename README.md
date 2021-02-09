1dd44ad71f281d6be942101ecd0e86f89766ae76
fgonzalezr1998 <fergonzaramos@yahoo.es>
2021-02-09 18:58:14 +0100

    [ros2cd.c] changes added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ros2cd
+
 ROS2 version of roscd command
 
 > NOTE: Work in progress
--- ros2cd.c
+++ ros2cd.c
@@ -6,26 +6,112 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #define NARGS 2
 #define MAXPATH 128
 #define PATHSFILE "/.ros2cd/paths.txt"
 
 int
-pkg_found(char * path)
+is_ros_pkg(char * path)
 {
-    return 1;
+    DIR *dir;
+    struct dirent *folder_entry;
+    int found;
+
+    dir = opendir(path);
+    if (dir == NULL) {
+        return 0;
+    }
+
+    found = 0;
+    do {
+        folder_entry = readdir(dir);
+        if (folder_entry != NULL) {
+            if (folder_entry->d_type == DT_REG) {
+                found = strncmp(folder_entry->d_name, 
+                    "package.xml", strlen(folder_entry->d_name)) == 0;
+            }
+        }
+    } while (folder_entry != NULL && !found);
+
+    return found;
+}
+
+int
+names_are_equals(char * path, char * dst_pkg)
+{
+    char * p_name;
+
+    p_name = strrchr(path, '/');
+    p_name = p_name + sizeof(char);
+    printf("%s\n", p_name);
+    return strncmp(p_name, dst_pkg, strlen(p_name)) == 0;
+}
+
+int
+pkg_ok(char * path, char * dst_pkg)
+{
+    if (is_ros_pkg(path)) {
+        return names_are_equals(path, dst_pkg);
+    }
+    return 0;
+}
+
+int
+pkg_found(char * path, char * dst_pkg, char *out_pkg)
+{
+    DIR *dir;
+    struct dirent *folder_entry;
+    char p[MAXPATH];
+    int found;
+
+    dir = opendir(path);
+    if (dir == NULL) {
+        return 0;
+    }
+
+    found = 0;
+    do {
+        memset(p, '\0', MAXPATH * sizeof(char));
+        folder_entry = readdir(dir);
+        if (folder_entry != NULL) {
+            if (folder_entry->d_type == DT_DIR) {
+                if (strcmp(folder_entry->d_name, ".") == 0 || 
+                    strcmp(folder_entry->d_name, "..") == 0) {
+                        continue;
+                    }
+                // Create the new sub-path:
+
+                memcpy(p, path, strlen(path) * sizeof(char));
+                strncat(p, "/", 1 * sizeof(char));
+                strncat(p, folder_entry->d_name, 
+                    strlen(folder_entry->d_name) * sizeof(char));
+                if(pkg_ok(p, dst_pkg)) {
+                    memcpy(out_pkg, p, strlen(p) * sizeof(char));
+                    found = 1;
+                } else {
+                    printf("Probando con: %s\n", p);
+                    found = pkg_found(p, dst_pkg, out_pkg);
+                }
+            }
+        }
+    } while (folder_entry != NULL && !found);
+
+    return found;
 }
 
 int
 move_to_pkg(int fd, char * dst_pkg)
 {
-    int bytes, finish, i;
+    int bytes, found, i;
     char c;
     char pkgs_path[MAXPATH];
+    char out_pkg[MAXPATH];
 
     i = 0;
-    finish = 0;
+    found = 0;
+    memset(out_pkg, '\0', MAXPATH * sizeof(char));
     do {
         bytes = read(fd, &c, 1);
         if (bytes < 0) {
@@ -39,12 +125,15 @@ move_to_pkg(int fd, char * dst_pkg)
         if (c == '\n' || bytes == 0) {
             pkgs_path[i] = '\0';
             i = 0;
-            if (pkg_found(pkgs_path)) {
-                // move to it
-                finish = 1;
+            if (pkg_found(pkgs_path, dst_pkg, out_pkg)) {
+                // move to 'out_pkg'!
+                found = 1;
             }
         }
-    } while(bytes > 0 && !finish);
+    } while(bytes > 0 && !found);
+
+    if (!found)
+        printf("%s\n", "[WARN] package not found!");
 
     return 1;
 }
